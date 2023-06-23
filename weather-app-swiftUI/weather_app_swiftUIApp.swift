ac3e84a60bce7f2e7a1d24a3e72e9295eb657ba5
devpark435 <devpark435@gmail.com>
2023-06-23 13:21:33 +0900

    Initial Commit

---
+++ weather-app-swiftUI/weather_app_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_app_swiftUIApp.swift
+//  weather-app-swiftUI
+//
+//  Created by 박현렬 on 2023/06/23.
+//
+
+import SwiftUI
+
+@main
+struct weather_app_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
